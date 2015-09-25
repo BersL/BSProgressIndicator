6202625e769ad45c61a901aee3f39ab280c8b566
BersL <AnqiLiu@hustunique.com>
2015-09-25 21:13:15 +0800

    Initial Commit

---
+++ BSProgressIndicator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BSProgressIndicator
+//
+//  Created by Bers on 15/9/25.
+//  Copyright © 2015年 Bers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BSProgressIndicator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BSProgressIndicator
+//
+//  Created by Bers on 15/9/25.
+//  Copyright © 2015年 Bers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
